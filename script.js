fd6650939a697b2a48b3a00004adcef7d9b6be48
Thomas <152648240+Platundi@users.noreply.github.com>
2024-01-27 21:18:06 +0100

    Implement UI Implemented basic UI for the player choice. Implemented result log.

--- script.js
+++ script.js
@@ -1,104 +1,128 @@
-alert("Best of Five, good luck :)");
+// alert("Best of Five, good luck :)");
 let gewonnen = 0;
 let verloren = 0;
 let x;
 
-function game() {
-  const playerSelection = getPlayerChoice();
-  const cpuSelection = getCPUchoice();
+let btnContainer = document.querySelector(".btnContainer");
 
-  // CPU-Input
-  function getCPUchoice() {
-    let rnd = Math.floor(Math.random() * 3);
-    switch (rnd) {
-      case 0:
-        return "Rock";
-      case 1:
-        return "Paper";
-      case 2:
-        return "Scissors";
+btnContainer.addEventListener("click", (event) => {
+  function game() {
+    let target = event.target;
+    const playerSelection = getPlayerChoice();
+    const cpuSelection = getCPUchoice();
+
+    // CPU-Input
+    function getCPUchoice() {
+      let rnd = Math.floor(Math.random() * 3);
+      switch (rnd) {
+        case 0:
+          return "Rock";
+        case 1:
+          return "Paper";
+        case 2:
+          return "Scissors";
+      }
     }
-  }
 
-  // Userinput
-  function getPlayerChoice() {
-    let isValid;
-    let playerSelection;
-    let restLetters;
+    // Userinput
+    function getPlayerChoice() {
+      // let isValid;
+      let playerSelection;
+      // let restLetters;
 
-    while (!isValid) {
-      playerSelection = prompt("Gib Rock, Paper oder Scissors ein");
-      restLetters = playerSelection.slice(1);
-      playerSelection =
-        playerSelection.charAt(0).toUpperCase() + restLetters.toLowerCase();
+      // while (!isValid) {
+      //   playerSelection = prompt("Gib Rock, Paper oder Scissors ein");
+      //   restLetters = playerSelection.slice(1);
+      //   playerSelection =
+      //     playerSelection.charAt(0).toUpperCase() + restLetters.toLowerCase();
 
-      if (
-        playerSelection == "Rock" ||
-        playerSelection == "Paper" ||
-        playerSelection == "Scissors"
-      ) {
-        isValid = true;
+      //   if (
+      //     playerSelection == "Rock" ||
+      //     playerSelection == "Paper" ||
+      //     playerSelection == "Scissors"
+      //   ) {
+      //     isValid = true;
+      //   } else {
+      //     alert("Falsche Eingabe. Bitte Rock, Paper oder Scissors eingeben!");
+      //   }
+      // }
+      console.log(target.id);
+      switch (target.id) {
+        case "schere":
+          playerSelection = "Scissors";
+          break;
+        case "stein":
+          playerSelection = "Rock";
+          break;
+        case "papier":
+          playerSelection = "Paper";
+          break;
+      }
+      return playerSelection;
+    }
+
+    // Spiellogik
+    function playRound(playerSelection, cpuSelection) {
+      if (playerSelection == cpuSelection) {
+        // alert("Unentschieden! Wiederholung");
+        return "Unentschieden";
       } else {
-        alert("Falsche Eingabe. Bitte Rock, Paper oder Scissors eingeben!");
+        switch (playerSelection) {
+          case "Rock":
+            if (cpuSelection == "Paper") {
+              return "Verloren";
+            } else if (cpuSelection == "Scissors") {
+              return "Gewonnen";
+            }
+            break;
+          case "Paper":
+            if (cpuSelection == "Rock") {
+              return "Gewonnen";
+            } else if (cpuSelection == "Scissors") {
+              return "Verloren";
+            }
+            break;
+          case "Scissors":
+            if (cpuSelection == "Rock") {
+              return "Verloren";
+            } else if (cpuSelection == "Paper") {
+              return "Gewonnen";
+            }
+        }
       }
     }
 
-    return playerSelection;
-  }
-
-  // Spiellogik
-  function playRound(playerSelection, cpuSelection) {
-    if (playerSelection == cpuSelection) {
-      alert("Unentschieden! Wiederholung");
-      return "Unentschieden";
+    // Ergebnisverarbeitung
+    let resultText = document.createElement("p");
+    let resultContainer = document.querySelector(".result");
+    let result = playRound(playerSelection, cpuSelection);
+    if (result == "Gewonnen") {
+      gewonnen++;
+      resultText.textContent = result;
+      resultContainer.appendChild(resultText);
+      // alert("Gewonnen, CPU hatte " + cpuSelection);
+      // if (gewonnen == 3) {
+      //   alert("Gewonnen!");
+      //   return 1;
+      // }
+    } else if (result == "Verloren") {
+      verloren++;
+      resultText.textContent = result;
+      resultContainer.appendChild(resultText);
+      // alert("Verloren, CPU hatte " + cpuSelection);
+      // if (verloren == 3) {
+      //   alert("Verloren");
+      //   return 1;
+      // }
     } else {
-      switch (playerSelection) {
-        case "Rock":
-          if (cpuSelection == "Paper") {
-            return "Verloren";
-          } else if (cpuSelection == "Scissors") {
-            return "Gewonnen";
-          }
-          break;
-        case "Paper":
-          if (cpuSelection == "Rock") {
-            return "Gewonnen";
-          } else if (cpuSelection == "Scissors") {
-            return "Verloren";
-          }
-          break;
-        case "Scissors":
-          if (cpuSelection == "Rock") {
-            return "Verloren";
-          } else if (cpuSelection == "Paper") {
-            return "Gewonnen";
-          }
-      }
+      resultText.textContent = result;
+      resultContainer.appendChild(resultText);
+      return 0;
     }
   }
 
-  // Ergebnisverarbeitung
-  let result = playRound(playerSelection, cpuSelection);
-  if (result == "Gewonnen") {
-    gewonnen++;
-    alert("Gewonnen, CPU hatte " + cpuSelection);
-    if (gewonnen == 3) {
-      alert("Gewonnen!");
-      return 1;
-    }
-  } else if (result == "Verloren") {
-    verloren++;
-    alert("Verloren, CPU hatte " + cpuSelection);
-    if (verloren == 3) {
-      alert("Verloren");
-      return 1;
-    }
-  } else {
-    return 0;
-  }
-}
-
-do {
+  // do {
   x = game();
-} while (x != 1);
-location.reload();
+  // } while (x != 1);
+  // location.reload();
+});
